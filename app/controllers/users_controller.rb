3a59d17f71f8b876c35222ff0c63e03f69916d1e
Alex Ramirez <r.alexramirez@outlook.com>
2016-08-17 22:51:39 -0400

    trying to build partials but can't get the form_for correct

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+
   def show
     user_id = params[:id] || current_user.id
     @user = User.find(user_id)
+  end
+
+  def item
+    @item = User.item
   end
 
 end
