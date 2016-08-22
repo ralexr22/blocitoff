6f9b635bec7c69818f5dea7bfc9d510634583439
Alex Ramirez <r.alexramirez@outlook.com>
2016-08-22 11:28:20 -0400

    Added an each loop to create multiple items and display them when added

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
   def create
-    @item = Item.new
+    @item = Item.new(name: params["item"]["name"])
+    @user = User.find(params[:user_id])
+    @item.user = current_user
+    @item.save
+    redirect_to user_path(@user)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,8 @@ class UsersController < ApplicationController
   def show
     user_id = params[:id] || current_user.id
     @user = User.find(user_id)
-  end
-
-  def item
-    @item = User.item
+    @item = Item.new
+    @items = @user.items 
   end
 
 end
