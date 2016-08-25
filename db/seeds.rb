58793ef3be7ab42e0a0945453158d25e9034b710
Alex Ramirez <r.alexramirez@outlook.com>
2016-08-25 17:31:14 -0400

    Using faker to seed app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,20 @@
+require 'faker'
 
+1.times do
+  User.create!(
+    email: Faker::Internet.email,
+    encrypted_password: Faker::Internet.password
+    )
+end
+
+user = current_user
+
+10.times do
+  Item.create!(
+   name: Faker::Lorem.words(true)
+  )
+end
+
+puts "Seed finished"
+puts "#{User.name} created"
+puts "#{Item.count} items created"
