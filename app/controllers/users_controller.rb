2536789a7184d0886044f61871455484632a4245
Alex Ramirez <r.alexramirez@outlook.com>
2016-09-13 17:43:32 -0400

    extra credit: making show page private

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
-
   def show
     user_id = params[:id] || current_user.id
     @user = User.find(user_id)
-    @item = Item.new
-    @items = @user.items 
-  end
 
+    # if the @user is not the current user, redirect to welcome page.
+    if current_user != @user
+      flash[:notice] = "This list is private"
+      redirect_to welcome_index_path
+
+    end
+
+
+    @item = Item.new
+    @items = @user.items
+  end
 end
